f029c17a6e4e94bb8b320d556e10bc104d1780ef
RA9T0R <phongphatbangkha@gmail.com>
2025-11-07 09:37:07 +0700

    Load real events

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,14 +3,14 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import {cacheLife} from "next/cache";
-import {events} from "@/lib/constants"; // This is only for static data testing
+// import {events} from "@/lib/constants"; // This is only for static data testing
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 const Home = async () => {
   'use cache';
   cacheLife('hours');
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
   return (
     <section className="mt-5">
